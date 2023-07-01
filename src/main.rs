60920d39e0c835c9c6cca3f35af73a97e62847cb
RDIO <1025760745@qq.com>
2023-07-01 16:19:59 +0800

    read from json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+use std::fs;
+use serde_json::{Result,Value};
 fn main() {
-    println!("Hello, world!");
+    // let arg: Vec<String> = env::args().collect();
+    // dbg!(arg);
+    let file_path="/home/archer/workspace/BEC/map.json";
+    let content = fs::read_to_string(file_path).expect(&format!("can't get the path {file_path}"));
+    let data:Value=serde_json::from_str(&content).expect(&format!("can't read json,file content :\n {content} \n"));
+    let layout = data["layout"].clone();
+    println!("{layout},\n{},\n{}",layout[1],layout[1][2]);
 }
