196facabb5d6238d8150db9b1c4c9920d4d65ea3
Woodman3 <1025760745@qq.com>
2023-07-08 16:40:07 +0800

    blockinfo as binary

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
-use std::env;
 use std::fs;
-use serde_json::{Result,Value};
+use std::error;
+// use serde_json::{Value};
+pub mod map;
+pub mod blockinfo;
+use map::Map;
+type Result<T> = std::result::Result<T,Box<dyn std::error::Error>>;
+// fn read_for_json(json_path:&str)->Result<Map>{
+//     let map_path=json_path.to_owned()+"map.json";
+//     let content = fs::read_to_string(map_path)?;
+//         // .expect(&format!("can't get the path {file_path}"));
+//     // let data:Value=serde_json::from_str(&content)?;
+//     // .expect(&format!("can't read json,file content :\n {content} \n"));
+//     let m:Map = serde_json::from_str(&content)?;
+//         // .unwrap();
+//     Ok(m)
+//     // println!("{}");
+//
+// }
 fn main() {
-    // let arg: Vec<String> = env::args().collect();
-    // dbg!(arg);
-    let file_path="/home/archer/workspace/BEC/map.json";
-    let content = fs::read_to_string(file_path).expect(&format!("can't get the path {file_path}"));
-    let data:Value=serde_json::from_str(&content).expect(&format!("can't read json,file content :\n {content} \n"));
-    let layout = data["layout"].clone();
-    println!("{layout},\n{},\n{}",layout[1],layout[1][2]);
+    // let m = crate::read_for_json("/home/archer/workspace/BEC/config/").unwrap();
+    // println!("{:?}",m);
+    let v=blockinfo::read_from_json("/home/archer/workspace/BEC/config/").unwrap();     
+    println!("{:?}",v);
 }
