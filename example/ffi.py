b268218810dfaa80dc31519258e3bd9da239bcbf
Woodman3 <1025760745@qq.com>
2024-04-14 23:30:42 +0800

    ffi change

--- example/ffi.py
+++ example/ffi.py
@@ -1,17 +1,19 @@
 from ctypes import * 
-dl=cdll.LoadLibrary
+dl=WinDLL
 dll=dl("./target/debug/wae.dll")
 buffer = create_string_buffer(b'\0',100)
 t=dll.init(b"./config")
 if(t==0):
-    # ptr=dll.get_obs()
-    # cp = c_char_p(ptr) 
-    # data=cp.value
-    # print(data)
+    ptr=dll.get_obs()
+    cp = c_char_p(ptr) 
+    data=cp.value.decode()
+    print(data)
     
-    dll.get_obs.restype=c_char_p
-    ptr = dll.get_obs()
-    print(ptr)
+    # dll.get_obs.restype=c_char_p
+    # ptr = dll.get_obs()
+    # print(ptr)
     # dll.free_str.argtypes=[c_char_p]
     # r=dll.free_str(ptr)
-    print(r)
+    # print(r)
+else:
+    print("init error")
