5470e83bdfd8601bb6fc6097fc92bfaed25822bb
Woodman3 <1025760745@qq.com>
2024-04-14 23:31:17 +0800

    ffi

--- example/ffi.py
+++ example/ffi.py
@@ -6,14 +6,13 @@ t=dll.init(b"./config")
 if(t==0):
     ptr=dll.get_obs()
     cp = c_char_p(ptr) 
-    data=cp.value.decode()
+    # data=cp.value.decode()
+    # print(data)
+    
+    dll.get_obs.restype=c_char_p
+    data = dll.get_obs()
+    print(ptr)
     print(data)
     
-    # dll.get_obs.restype=c_char_p
-    # ptr = dll.get_obs()
-    # print(ptr)
-    # dll.free_str.argtypes=[c_char_p]
-    # r=dll.free_str(ptr)
-    # print(r)
 else:
     print("init error")
