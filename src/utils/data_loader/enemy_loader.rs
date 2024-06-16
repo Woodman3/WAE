12bd99914da10b4e081443f31f55169410e7ecb5
Woodman3 <1025760745@qq.com>
2024-06-16 11:25:12 +0800

    chore: Update enemy_loader.rs to include missing enemy attributes

--- src/utils/data_loader/enemy_loader.rs
+++ src/utils/data_loader/enemy_loader.rs
@@ -15,29 +15,30 @@ struct OfficalEnemyValue{
 #[derive(Deserialize,Default,Debug)]
 struct OfficalEnemyData{
     pub(super) name:OfficalEnemyDataTemplate<String>,
-    // pub(super) applyWay:OfficalEnemyDataTemplate<String>,
-    // pub(super) motion:OfficalEnemyDataTemplate<String>,
-    // pub(super) lifePointReduce:OfficalEnemyDataTemplate<u32>,
-    // pub(super) attributes:OfficalEnemyDataTemplate<String>,
+    pub(super) applyWay:OfficalEnemyDataTemplate<String>,
+    pub(super) motion:OfficalEnemyDataTemplate<String>,
+    pub(super) lifePointReduce:OfficalEnemyDataTemplate<u32>,
+    pub(super) attributes:OfficalEnemyAttribute,
 }
 
+#[derive(Deserialize,Default,Debug)]
 struct OfficalEnemyAttribute{
-    pub(super) maxHp:OfficalEnemyDataTemplate<u32>,
-    pub(super) atk:OfficalEnemyDataTemplate<u32>,
-    pub(super) def:OfficalEnemyDataTemplate<u32>,
-    pub(super) magidResistance:OfficalEnemyDataTemplate<f32>,
-    pub(super) cost:OfficalEnemyDataTemplate<u32>,
-    pub(super) blockCnt:OfficalEnemyDataTemplate<u32>,
+    pub(super) maxHp:OfficalEnemyDataTemplate<i32>,
+    pub(super) atk:OfficalEnemyDataTemplate<i32>,
+    pub(super) def:OfficalEnemyDataTemplate<i32>,
+    pub(super) magicResistance:OfficalEnemyDataTemplate<f32>,
+    pub(super) cost:OfficalEnemyDataTemplate<i32>,
+    pub(super) blockCnt:OfficalEnemyDataTemplate<i32>,
     pub(super) moveSpeed:OfficalEnemyDataTemplate<f32>,
     pub(super) attackSpeed:OfficalEnemyDataTemplate<f32>,
     pub(super) baseAttackTime:OfficalEnemyDataTemplate<f32>,
-    pub(super) respawnTime:OfficalEnemyDataTemplate<u32>,
+    pub(super) respawnTime:OfficalEnemyDataTemplate<i32>,
     pub(super) hpRecoveryPerSec:OfficalEnemyDataTemplate<f32>,
     pub(super) spRecoveryPerSec:OfficalEnemyDataTemplate<f32>,
-    pub(super) maxDeployCount:OfficalEnemyDataTemplate<u32>,
-    pub(super) massLevel:OfficalEnemyDataTemplate<u32>,
-    pub(super) baseForceLevel:OfficalEnemyDataTemplate<u32>,
-    pub(super) tauntLevel:OfficalEnemyDataTemplate<u32>,
+    pub(super) maxDeployCount:OfficalEnemyDataTemplate<i32>,
+    pub(super) massLevel:OfficalEnemyDataTemplate<i32>,
+    pub(super) baseForceLevel:OfficalEnemyDataTemplate<i32>,
+    pub(super) tauntLevel:OfficalEnemyDataTemplate<i32>,
     pub(super) epDamageResistance:OfficalEnemyDataTemplate<f32>,
     pub(super) epResistance:OfficalEnemyDataTemplate<f32>,
     pub(super) damageHitratePhysical:OfficalEnemyDataTemplate<f32>,
@@ -54,7 +55,7 @@ struct OfficalEnemyAttribute{
 struct OfficalEnemyDataTemplate<T>
 {
     pub(super) m_defined:bool,
-    pub(super) m_value:T,
+    pub(super) m_value:Option<T>,
 }
 
 #[cfg(test)]
