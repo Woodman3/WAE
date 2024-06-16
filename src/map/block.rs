880bcbe2634ec80939126127954403a67a72d193
Woodman3 <1025760745@qq.com>
2024-06-16 22:39:14 +0800

    chore: Update block.rs to make TileInfo struct and related constants public

--- src/map/block.rs
+++ src/map/block.rs
@@ -10,27 +10,27 @@ use super::Map;
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 /// | ... | 1 | 1 | 3 | | 8 |
 /// | save | can pass | has deploy | deploy limit | id |
-type LayoutCode=u64;
+pub(crate) type LayoutCode=u64;
 #[derive(Serialize,Deserialize,Debug)]
-struct BlockInfo{
+struct TileInfo{
     id:u64,
     name:String,
     can_deploy:String,
     can_pass:bool  
 }
 
-const ID_MASK_LEN:u32 = 8;
-const DEPLOY_MASK_LEN:u32=3;
+pub(crate) const ID_MASK_LEN:u32 = 8;
+pub(crate) const DEPLOY_MASK_LEN:u32=3;
 
-const ID_MASK:u64=2_u64.pow(ID_MASK_LEN)-1;
-const DEPLOY_MASK:u64=2_u64.pow(ID_MASK_LEN+DEPLOY_MASK_LEN)-1-ID_MASK;
-const PASS_MASK:u64=2_u64.pow(ID_MASK_LEN+DEPLOY_MASK_LEN);
+pub(crate) const ID_MASK:u64=2_u64.pow(ID_MASK_LEN)-1;
+pub(crate) const DEPLOY_MASK:u64=2_u64.pow(ID_MASK_LEN+DEPLOY_MASK_LEN)-1-ID_MASK;
+pub(crate) const PASS_MASK:u64=2_u64.pow(ID_MASK_LEN+DEPLOY_MASK_LEN);
 
-const DEPLOY_HIGH:u64=1<<ID_MASK_LEN;
-const DEPLOY_LOW:u64=2<<ID_MASK_LEN;
+pub(crate) const DEPLOY_HIGH:u64=1<<ID_MASK_LEN;
+pub(crate) const DEPLOY_LOW:u64=2<<ID_MASK_LEN;
 
-impl From<BlockInfo> for LayoutCode{
-    fn from(value: BlockInfo) -> Self {
+impl From<TileInfo> for LayoutCode{
+    fn from(value: TileInfo) -> Self {
         let mut r:u64=0;
         r|=value.id|ID_MASK;
         match value.can_deploy.as_str() {
@@ -46,7 +46,7 @@ impl From<BlockInfo> for LayoutCode{
 }
 
 pub(super) fn generate_layout(v:&Value,mut m:Map)-> Result<Map>{
-    let temp:HashMap<String,BlockInfo> = from_value(v["block"].clone())?;
+    let temp:HashMap<String,TileInfo> = from_value(v["block"].clone())?;
     let mut table = HashMap::<u64,LayoutCode>::new();
     for (_,b) in temp{
         table.insert(b.id,b.into());
