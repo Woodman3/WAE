11289add6c9d7432aa4b336a8141ea8193dbe1cd
Woodman3 <1025760745@qq.com>
2024-08-30 10:57:18 +0800

    lgtm

--- src/unit/enemy/enemy_mission.rs
+++ src/unit/enemy/enemy_mission.rs
@@ -8,21 +8,21 @@ impl Enemy{
     fn skill(&mut self,f:&mut Frame){
         self.skills.step(f);
     }
-    // fn generate_default_attack_skill(&mut self) {
-    //     let mut s = Skill::default();
-    //     s.duration = self.stage.attack_time;
-    //     let d = effect::FixedDamage {
-    //         value: self.stage.atk,
-    //         damage_type: self.stage.damage_type.clone(),
-    //     };
-    //     let se = ToOperatorSkill {
-    //         target: Vec::new(),
-    //         target_num: 1,
-    //         effect: effect::Effect::FixedDamage(d),
-    //         attack_type: self.stage.attack_type,
-    //         search_scope: Option::from(self.search_scope.clone()),
-    //     };
-    //     s.skill_entity = SkillEntity::ToOperatorSkill(se);
-    //     self.skills.skill_block.push(s);
-    // }
+    fn generate_default_attack_skill(&mut self) {
+        let mut s = Skill::default();
+        s.duration = self.stage.attack_time;
+        let d = effect::FixedDamage {
+            value: self.stage.atk,
+            damage_type: self.stage.damage_type.clone(),
+        };
+        let se = ToOperatorSkill {
+            target: Vec::new(),
+            target_num: 1,
+            effect: effect::Effect::FixedDamage(d),
+            attack_type: self.stage.attack_type,
+            search_scope: Option::from(self.stage.scope.clone()),
+        };
+        s.skill_entity = SkillEntity::ToOperatorSkill(se);
+        self.skills.skill_block.push(s);
+    }
 }
